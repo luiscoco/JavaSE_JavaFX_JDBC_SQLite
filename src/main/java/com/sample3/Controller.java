c71299e7e2037249574400637c76b4f63137c4b5
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-07-20 16:22:00 +0200

    changing update artist texbox styles

--- src/main/java/com/sample3/Controller.java
+++ src/main/java/com/sample3/Controller.java
@@ -7,6 +7,9 @@ import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
 import com.sample3.model.Album;
 import com.sample3.model.Artist;
 import com.sample3.model.Datasource;
@@ -19,7 +22,25 @@ public class Controller {
     @FXML
     private ProgressBar progressBar;
 
-    @FXML
+     @FXML
+    private TextField newArtistNameField; // Newly added TextField for the new artist name
+
+    public void initialize() {
+        newArtistNameField.sceneProperty().addListener((observable, oldScene, newScene) -> {
+            if (newScene != null) {
+                BorderPane root = (BorderPane) newScene.getRoot();
+                double halfWidth = root.getWidth() / 2;
+                newArtistNameField.setPrefWidth(halfWidth);
+
+                // Add a listener to adjust the width if the window size changes
+                root.widthProperty().addListener((widthObservable, oldWidth, newWidth) -> {
+                    double newHalfWidth = newWidth.doubleValue() / 2;
+                    newArtistNameField.setPrefWidth(newHalfWidth);
+                });
+            }
+        });
+    }
+
     public void listArtists() {
         Task<ObservableList<Artist>> task = new GetAllArtistsTask();
         artistTable.itemsProperty().bind(task.valueProperty());
@@ -61,22 +82,34 @@ public class Controller {
             return;
         }
 
+        // Get the new artist name from the text field
+        String newArtistName = newArtistNameField.getText();
+
         Task<Boolean> task = new Task<Boolean>() {
             @Override
             protected Boolean call() throws Exception {
-                return Datasource.getInstance().updateArtistName(artist.getId(), "AC/DC");
+                // Perform the update with the new artist name
+                return Datasource.getInstance().updateArtistName(artist.getId(), newArtistName);
             }
         };
 
+        // Show the progress bar during the update process
+        progressBar.setVisible(true);
+
         task.setOnSucceeded(e -> {
             if (task.getValue()) {
-                artist.setName("AC/DC");
+                artist.setName(newArtistName);
                 artistTable.refresh();
             }
+            // Hide the progress bar after the update is done
+            progressBar.setVisible(false);
         });
+
+        task.setOnFailed(e -> progressBar.setVisible(false));
 
         new Thread(task).start();
     }
+
 }
 
 class GetAllArtistsTask extends Task<ObservableList<Artist>> {
